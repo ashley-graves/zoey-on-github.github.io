496b804c34feecb124b7772a1b42c7426ba8e5f7
Ashley Graves <yuki@possum.city>
2024-11-17 05:10:25 +0100

    horror

--- generateImagePage.py
+++ generateImagePage.py
@@ -1,11 +1,35 @@
 import os
 import sys
+import mimetypes
+from pyffmpeg import FFmpeg
+from subprocess import run
+
+directory = os.fsdecode(sys.argv[2])
+
+f = open("head.html", "r", encoding="utf-8")
+head = f.read()
+f.close()
 
 f = open(sys.argv[1], "w", encoding="utf-8")
-f.write("    <link rel=\"stylesheet\" href=\"wallpaper.css\">"+"\n")
-directory = os.fsdecode(sys.argv[2])
+f.write(head)
+f.write('\n\n<body>\n')
+
+
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    # with open("wallpapers/" + filename) as fp2:
-    f.write("<img src=\"" + sys.argv[2] + "/" + filename + "\">" + "\n")
+    thumbfile = filename[0:filename.rindex(".")] + ".jpg"
+
+    path = os.path.join(directory, filename)
+    thumb = os.path.join("thumbs", thumbfile)
+
+    p = run(executable='ffmpeg', args=["", "-n", "-i", path, "-vf", "scale=100:100:force_original_aspect_ratio=increase", "-frames:v", "1", thumb])
+    if p.returncode != 0:
+        print("[!!!!!] ERROR: NON-ZERO EXIT CODE")
+        exit()
+
+    f.write(('  <a href="%s">\n') % path)
+    f.write(('      <img src="%s" />\n') % thumb)
+    f.write(('  </a>\n'))
+
+f.write('</body>\n\n</html>')
 f.close()
